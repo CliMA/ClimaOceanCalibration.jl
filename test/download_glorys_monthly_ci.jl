58b3349de30b24e83a6075fea1440a1a8d0fc725
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2025-09-23 18:08:59 -0700

    using Downloads.download

--- test/download_glorys_monthly_ci.jl
+++ test/download_glorys_monthly_ci.jl
@@ -1,8 +1,10 @@
+using Downloads
+
 dir = "./GLORYS_data"
 mkpath(dir)
 zipped_filepath = joinpath(dir, "glorys_data.zip")
 
-download("https://figshare.com/ndownloader/articles/30193228/versions/1", zipped_filepath)
+Downloads.download("https://figshare.com/ndownloader/articles/30193228/versions/1", zipped_filepath)
 
 run(`unzip -o $zipped_filepath -d $dir`)
 
