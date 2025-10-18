38b49847b9beb44c69e111257f9e63feac5c902a
Xin Kai Lee <xinkai@mit.edu>
2025-10-17 20:18:40 -0700

    Add buoyancy model initialization and save averaged buoyancy field timeseries

--- examples/GM_calibration/average_ECCO_data.jl
+++ examples/GM_calibration/average_ECCO_data.jl
@@ -1,11 +1,12 @@
 using ClimaOcean
 using Oceananigans
 using Oceananigans.Units
+using SeawaterPolynomials.TEOS10
 using ClimaOcean.DataWrangling
 using Printf
 using Dates
 using CUDA
-using ClimaOceanCalibration.DataWrangling: TimeAverageOperator, AveragedFieldTimeSeries, save_averaged_fieldtimeseries
+using ClimaOceanCalibration.DataWrangling: TimeAverageOperator, AveragedFieldTimeSeries, save_averaged_fieldtimeseries, TimeAverageBuoyancyOperator
 
 Nx, Ny, Nz = (180, 84, 100)
 z_faces = ExponentialDiscretization(Nz, -6000, 0; scale=1800)
@@ -23,6 +24,8 @@ dir = joinpath(homedir(), "ECCO_data")
 mkpath(dir)
 start_dates = [DateTime(1992, 1, 1), DateTime(2002, 1, 1)]
 
+buoyancy_model = SeawaterBuoyancy(equation_of_state=TEOS10EquationOfState())
+
 for start_date in start_dates
     end_date = start_date + Year(10) - Month(1)
 
@@ -38,6 +41,9 @@ for start_date in start_dates
     S_averaging = TimeAverageOperator(S_data)
     S_averaged_fts = AveragedFieldTimeSeries(S_averaging(S_data), S_averaging, nothing)
 
+    b_averaging = TimeAverageBuoyancyOperator(T_data)
+    b_averaged_fts = AveragedFieldTimeSeries(b_averaging(T, S, grid, buoyancy_model), b_averaging, nothing)
+
     prefix = "10yearaverage_2degree"
     date_str = replace(string(start_date), ":" => "-")
 
@@ -48,8 +54,10 @@ for start_date in start_dates
 
     T_filepath = joinpath(SAVE_PATH, "T.jld2")
     S_filepath = joinpath(SAVE_PATH, "S.jld2")
+    b_filepath = joinpath(SAVE_PATH, "b.jld2")
 
     save_averaged_fieldtimeseries(T_averaged_fts, T, filename=T_filepath, overwrite_existing=true)
     save_averaged_fieldtimeseries(S_averaged_fts, S, filename=S_filepath, overwrite_existing=true)
+    save_averaged_fieldtimeseries(b_averaged_fts, T, filename=b_filepath, overwrite_existing=true)
 end
 
