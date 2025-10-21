0be5dda022a481750c345dc680e8d5ffa38c131c
Xin Kai Lee <xinkai@mit.edu>
2025-10-21 10:30:23 -0700

    fix: update directory path for forcing data to ECCO data in one degree OMIP calibration

--- examples/GM_calibration_1degforwardmodel/one_degree_omip.jl
+++ examples/GM_calibration_1degforwardmodel/one_degree_omip.jl
@@ -79,7 +79,7 @@ function run_gm_calibration_omip(κ_skew, κ_symmetric, config_dict)
 
         closure = (obl_closure, VerticalScalarDiffusivity(κ=1e-5, ν=3e-4), eddy_closure)
 
-        dir = joinpath(homedir(), "forcing_data_half_degree")
+        dir = joinpath(homedir(), "ECCO_data")
         mkpath(dir)
 
         start_date = DateTime(start_year, 1, 1)
